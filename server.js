2e6c4e6964c2a76743c6bdec4a52040f3f112472
isabellafernandes27 <45519012+isabellafernandes27@users.noreply.github.com>
2018-12-02 11:10:18 -0700

    Finish scraper

--- server.js
+++ server.js
@@ -14,6 +14,48 @@ app.get('/', (req, res) => {
 });
 
 // your endpoint
+app.get('/pets', (req, res) => {
+  res.send("Dogs!");
+});
+app.get('/:name', (req, res) => {
+  var name = req.params.name;
+
+  res.send(`Hi, ${name}!`);
+});
+
+app.get('/hedgie/:keyword', (req, res) => {
+  var keyword = req.params.keyword;
+
+function findHedgieImage(keyword) {
+ var nightmare = Nightmare({ show: true });
+ return nightmare
+      .goto('https://www.google.com')
+      .insert('input[title="search"]',`hedgehog ${keyword}` )
+      .click('input[value="Google Search"]')
+      .wait('a.q.qs')
+      .click('a.q.qs')
+      .wait('div#res.med')
+      .evaluate(function() {
+        var photoDivs= document.querySelectorAll('img.rg_ic');
+        var list= [].slice.call(photoDivs);
+        return list.map(function(div) {
+          return div.src;
+        });
+      })
+      .end()
+      .then(function(result){
+        return result.slice(1,5);
+      })
+      .then(function(images){
+        res.json(images);
+      })
+      .catch (function(error) {
+        console.error('Search failed', error);
+      })
+      }
+      findHedgieImage(keyword);
+    })
+
 
 // scraper endpoint
 
